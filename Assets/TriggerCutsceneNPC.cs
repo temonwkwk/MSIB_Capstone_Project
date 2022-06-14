5416d08871d4d311109a1f7c678956c0c61ea9c6
zuzadgg <zuzadgg@gmail.com>
2022-06-14 10:21:29 +0700

    Add Npc,Anim,Cutscene..
    + NPC
    + Animasi NPC
    + Cutscene ketika trigger NPC

---
+++ Assets/TriggerCutsceneNPC.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCutsceneNPC : MonoBehaviour
+{
+
+    [SerializeField] private GameObject timelineCutscene1;  //Condition = 0/3
+    [SerializeField] private GameObject timelineCutscene2;  //Condition = 3/3
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if(GameData.instance.progressData == 0)
+            {
+                timelineCutscene1.SetActive(true);
+            }
+
+            if (GameData.instance.progressData == 3)
+            {
+                timelineCutscene2.SetActive(true);
+            }
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
