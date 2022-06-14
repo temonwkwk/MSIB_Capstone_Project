1dd6bb820bf89dc00f5e994e1b8ff0e463da238c
temonwkwk <fachrulreza4@gmail.com>
2022-06-14 15:22:54 +0700

    Warna caping berubah seiring power berganti

--- Assets/Script/Player/PowerIndicator.cs
+++ Assets/Script/Player/PowerIndicator.cs
@@ -6,9 +6,11 @@ public class PowerIndicator : MonoBehaviour
 {
     public float power = 0;
     public float maxPower = 3;
+    public GameObject[] ppower;
 
     void Start()
     {
+        ppower = GameObject.FindGameObjectsWithTag("Hat");
     }
 
     void Update()
@@ -23,6 +25,33 @@ public class PowerIndicator : MonoBehaviour
         if (power > maxPower)
         {
             power = 0;
+        }
+
+        switch (power)
+        {
+            case 0:
+            ppower[0].SetActive(false);
+            ppower[1].SetActive(false);
+            ppower[2].SetActive(false);
+            break;
+
+            case 1:
+            ppower[0].SetActive(true);
+            ppower[1].SetActive(false);
+            ppower[2].SetActive(false);
+            break;
+            
+            case 2:
+            ppower[0].SetActive(false);
+            ppower[1].SetActive(true);
+            ppower[2].SetActive(false);
+            break;
+
+            case 3:
+            ppower[0].SetActive(false);
+            ppower[1].SetActive(false);
+            ppower[2].SetActive(true);
+            break;
         }
     }
 }
